375a3fef7c6eb645f7bfbdf55f5a3ef2f6da86dd
mfglr <mfgglr@outlook.com>
2025-05-11 11:05:03 +0300

    Second step.

--- LoopsPratik1/Program.cs
+++ LoopsPratik1/Program.cs
@@ -1,2 +1,7 @@
 ﻿for (int i = 0; i < 10; i++)
     Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+
+Console.WriteLine("***************************************************************");
+
+for (int i = 0; i < 20; i++)
+    Console.WriteLine(i);
